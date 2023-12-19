2dfb895d7b832f6c167450df616e6383cb1bfb5f
henry edeh <henrydynamite25@gmail.com>
2023-12-19 09:59:43 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _abs - function to print absolut value
+ *  @n: function parameter
+ *  Return: always 0;
+ */
+int _abs(int n)
+
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+
+
+}
